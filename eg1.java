106c50c4603eefadf6ae790b72c75df04b293f9d
piyush dhakad <piyushdhakad08@gmail.com>
2022-06-19 13:54:58 +0530

    create New eg2.java

--- eg1.java
+++ eg1.java
@@ -1,10 +1,11 @@
 class Eg1 {
 public static void main(String args[]){
 
-int a,b;
+int a,b,c;
 System.out.println("Enter Two Number when run class file {java Eg1 3 5}");
 a = Integer.parseInt(args[0]);
 b = Integer.parseInt(args[1]);
-System.out.println("a+b = "+(a+b));
+c=a+b;
+System.out.println("a+b = "+c);
 }
 }
