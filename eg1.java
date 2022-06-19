de066e04037634e29e3ad660013a703c94f683eb
piyush dhakad <piyushdhakad08@gmail.com>
2022-06-19 13:42:25 +0530

    initial commit

---
+++ eg1.java
@@ -0,0 +1,10 @@
+class Eg1 {
+public static void main(String args[]){
+
+int a,b;
+System.out.println("Enter Two Number when run class file {java Eg1 3 5}");
+a = Integer.parseInt(args[0]);
+b = Integer.parseInt(args[1]);
+System.out.println("a+b = "+(a+b));
+}
+}
